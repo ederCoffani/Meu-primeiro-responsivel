14a32d630a5a8425ec8bf4656189ceb811a564b6
Eder Coffani <edcoffani@gmail.com>
2017-09-12 19:20:55 -0300

    insersão das pages inicio, sobre, portfolio e contato

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
-//task para sass
+
+// task para o sass
 gulp.task('sass', function() {
     return gulp.src('sass/**/*.sass')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'))
-});
-//task para watch
-gulp.task('watch', function() {
-	gulp.watch('sass/**/*.sass', ['sass'])
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('css'));
 });
 
+// task para watch
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.sass', ['sass']);
+});
 
-// task default
+// task default gulp
 gulp.task('default', ['sass', 'watch']);
